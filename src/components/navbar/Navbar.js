88c96f22058a63e2c8d1b8681e60a919a86428e7
NeelClaudel <neel.claudel@gmail.com>
2022-12-13 04:48:55 +0100

    modified:   src/components/navbar/Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -12,13 +12,28 @@ const Navbar = () => {
         <h1 style={{ marginLeft: "1rem", color: "#00d8ff" }}>depass.</h1>
         <ul className={click ? "nav active" : "nav"}>
           <li className="nav-item">
-            <a href="/">Investir</a>
+            <a
+              href="https://dexscreener.com/polygon/0x8bb12292ec2cfc5cf7811457afd383b1d1719502"
+              target="blank"
+            >
+              Investir
+            </a>
           </li>
           <li className="nav-item">
-            <a href="/">Billetterie</a>
+            <a
+              href="https://medium.com/@neel.claudel/depass-r%C3%A9volutionne-la-billetterie-944f64a119dc"
+              target="blank"
+            >
+              Billetterie
+            </a>
           </li>
           <li className="nav-item">
-            <a href="/">Blockchain</a>
+            <a
+              href="https://polygonscan.com/address/0x0fc54445e1187d57b346a81c9b6e974dc810bead"
+              target="blank"
+            >
+              Token
+            </a>
           </li>
           <li className="nav-item">
             <a href="/">NFTs</a>
